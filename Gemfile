4e3a5207b255e787d22d671c84c695d1e9854533
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-05 18:52:58 +0900

    Add bootstrap & partial form

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem "stimulus-rails"
 gem "cssbundling-rails"
 gem "ransack"
 gem "kaminari"
-gem "ransack"
+gem "bootstrap_form"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,9 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  # nameが空でないことを検証
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # numericalityのオプションで、ageが整数であり、0以上であることを検証
+
   def self.ransackable_attributes(auth_object = nil)
     %w[id name age created_at updated_at]
   end
@@ -6,4 +11,6 @@ class Cat < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     []
   end
+
+
 end
